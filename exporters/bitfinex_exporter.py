99e0ea8a372110bb01b2e96063aa590a9583198d
Alex Thomae <alex@thom.ae>
2018-02-01 15:56:47 +0000

    Closes #3: Rewrite `bitfinex_exporter` to use `ccxt`

--- exporters/bitfinex_exporter.py
+++ exporters/bitfinex_exporter.py
@@ -7,9 +7,7 @@ import yaml
 import sys
 import requests
 import json
-import base64
-import hashlib
-import hmac
+import ccxt
 from prometheus_client import write_to_textfile, start_http_server
 from prometheus_client.core import REGISTRY, GaugeMetricFamily, CounterMetricFamily
 
@@ -30,7 +28,6 @@ def _settings():
             'api_secret': None,
             'export': 'text',
             'listen_port': 9300,
-            'url': 'https://api.bitfinex.com',
         },
     }
     config_file = '/etc/bitfinex_exporter/bitfinex_exporter.yaml'
@@ -47,8 +44,6 @@ def _settings():
             settings['bitfinex_exporter']['api_key'] = cfg['bitfinex_exporter']['api_key']
         if cfg['bitfinex_exporter'].get('api_secret'):
             settings['bitfinex_exporter']['api_secret'] = cfg['bitfinex_exporter']['api_secret']
-        if cfg['bitfinex_exporter'].get('url'):
-            settings['bitfinex_exporter']['url'] = cfg['bitfinex_exporter']['url']
         if cfg['bitfinex_exporter'].get('export') in ['text', 'http']:
             settings['bitfinex_exporter']['export'] = cfg['bitfinex_exporter']['export']
         if cfg['bitfinex_exporter'].get('listen_port'):
@@ -56,121 +51,57 @@ def _settings():
 
 
 class BitfinexCollector:
-    symbols = []
     rates = {}
     accounts = {}
+    hasApiCredentials = False
 
     def __init__(self):
-        self._getSymbols()
-
-    def _nonce(self):
-        """
-        Returns a nonce
-        Used in authentication
-        """
-        return str(int(round(time.time() * 1000)))
-
-    def _headers(self, path, nonce, body):
-        signature = "/api" + path + nonce + body
-        log.debug("Signing: {}".format(signature))
-        signature = hmac.new(
-            bytes(settings['bitfinex_exporter']['api_secret'], 'utf-8'),
-            bytes(signature, 'utf-8'),
-            hashlib.sha384
-        ).hexdigest()
-
-        return {
-            "bfx-nonce": nonce,
-            "bfx-apikey": settings['bitfinex_exporter']['api_key'],
-            "bfx-signature": signature,
-            "content-type": "application/json"
-        }
-
-    def _getSymbols(self):
-        """
-        Gets the list of symbols
-
-        Unfortunately, this only works over the v1 API
-        """
-        self.symbols = []
-        path = "/v1/symbols"
-        r = requests.get(settings['bitfinex_exporter']['url'] + path, verify=True)
-        if r and r.status_code == 200:
-            for symbol in r.json():
-                if symbol.upper() not in self.symbols:
-                    self.symbols.append(symbol.upper())
+        self.bitfinex = ccxt.bitfinex()
+        if (settings['bitfinex_exporter'].get('api_key') and (settings['bitfinex_exporter'].get('api_secret'))):
+            self.bitfinex.apiKey = settings['bitfinex_exporter'].get('api_key')
+            self.bitfinex.secret = settings['bitfinex_exporter'].get('api_secret')
+            self.hasApiCredentials = True
         else:
-            log.warning('Could not retrieve symbols. Response follows.')
-            log.warning(r.headers)
-            log.warning(r.json())
+            log.warning('API credentials not found.')
 
-        log.debug('Found the following symbols: {}'.format(self.symbols))
+    def _getTickers(self):
+        """
+        Gets the price ticker.
+        """
+        self.bitfinex.loadMarkets(True)
 
-    def _getAccountBalances(self):
-        nonce = self._nonce()
-        rawBody = json.dumps({})
-        path = "/v2/auth/r/wallets"
-        headers = self._headers(path, nonce, rawBody)
+        tickers = self.bitfinex.fetch_tickers()
 
-        log.debug(settings['bitfinex_exporter']['url'] + path)
-        log.debug('Headers: {}'.format(headers))
+        for ticker in tickers:
+            currencies = ticker.split('/')
+            pair = {
+                'source_currency': currencies[0],
+                'target_currency': currencies[1],
+                'value': float(tickers[ticker]['last']),
+            }
 
-        r = requests.post(settings['bitfinex_exporter']['url'] + path, headers=headers, data=rawBody, verify=True)
+            self.rates.update({
+                '{}'.format(ticker): pair
+            })
 
-        if r and r.status_code == 200:
-            for account in r.json():
-                if isinstance(account, list):
-                    self.accounts.update({
-                        '{}-{}'.format(account[0], account[1]): {
-                            'balance': account[2],
-                            'currency': account[1],
-                            'account': account[0],
-                        }
-                    })
-                else:
-                    log.warning('Invalid object in response.')
-                    log.warning(account)
-        else:
-            log.warning('Could not retrieve data. Response follows.')
-            log.warning(r.headers)
-            log.warning(r.text)
-
-    def _getExchangeRates(self):
-        self._getSymbols()
-        if self.symbols:
-            get_symbols = []
-            for symbol in self.symbols:
-                get_symbols.append('t{}'.format(symbol))
-            symbols_string = ','.join(get_symbols)
-            path = "/v2/tickers"
-
-            log.debug('Symbols: {}'.format(symbols_string))
-            r = requests.get(
-                settings['bitfinex_exporter']['url'] + path,
-                params={'symbols': symbols_string},
-                verify=True
-            )
-
-            if r and r.status_code == 200:
-                for ticker in r.json():
-                    if isinstance(ticker, list):
-                        source_currency = ticker[0][1:4]
-                        target_currency = ticker[0][-3:]
-                        self.rates.update({
-                            '{}-{}'.format(source_currency, target_currency): {
-                                'source_currency': source_currency,
-                                'target_currency': target_currency,
-                                'value': ticker[7],
-                            }
-                        })
-                    else:
-                        log.warning('Invalid object in response for ticker.')
-                        log.warning(ticker)
-            else:
-                log.warning('Could not retrieve ticker data. Response follows.')
-                log.warning(r.headers)
-                log.warning(r.text)
         log.debug('Found the following ticker rates: {}'.format(self.rates))
+
+    def _getAccounts(self):
+        if self.hasApiCredentials:
+            accounts = self.bitfinex.fetch_balance()
+            self.accounts = {}
+            if accounts.get('free'):
+                for currency in accounts['free']:
+                    if not self.accounts.get(currency):
+                        self.accounts.update({currency: {}})
+                    self.accounts[currency].update({'free': accounts['free'][currency]})
+            if accounts.get('used'):
+                for currency in accounts['used']:
+                    if not self.accounts.get(currency):
+                        self.accounts.update({currency: {}})
+                    self.accounts[currency].update({'used': accounts['used'][currency]})
+
+        log.debug('Found the following accounts: {}'.format(self.accounts))
 
     def collect(self):
         metrics = {
@@ -179,27 +110,13 @@ class BitfinexCollector:
                 'Current exchange rates',
                 labels=['source_currency', 'target_currency', 'exchange']
             ),
-        }
-
-        if settings['bitfinex_exporter'].get('api_key'):
-            metrics.update({'account_balance': GaugeMetricFamily(
+            'account_balance': GaugeMetricFamily(
                 'account_balance',
                 'Account Balance',
                 labels=['source_currency', 'currency', 'account', 'type']
-            )})
-            self._getAccountBalances()
-            for account in self.accounts:
-                metrics['account_balance'].add_metric(
-                    value=(self.accounts[account]['balance']),
-                    labels=[
-                        self.accounts[account]['currency'],
-                        self.accounts[account]['currency'],
-                        self.accounts[account]['account'],
-                        'bitfinex'
-                    ]
-                )
-
-        self._getExchangeRates()
+            ),
+        }
+        self._getTickers()
         for rate in self.rates:
             metrics['exchange_rate'].add_metric(
                 value=self.rates[rate]['value'],
@@ -210,13 +127,27 @@ class BitfinexCollector:
                 ]
             )
 
+        self._getAccounts()
+        for currency in self.accounts:
+            for account_type in self.accounts[currency]:  # free / used
+                if (self.accounts[currency][account_type] > 0):
+                    metrics['account_balance'].add_metric(
+                        value=(self.accounts[currency][account_type]),
+                        labels=[
+                            currency,
+                            currency,
+                            account_type,
+                            'bitfinex'
+                        ]
+                    )
+
         for m in metrics.values():
             yield m
 
 
 def _collect_to_text():
+    e = BitfinexCollector()
     while True:
-        e = BitfinexCollector()
         write_to_textfile('{0}/bitfinex_exporter.prom'.format(settings['bitfinex_exporter']['prom_folder']), e)
         time.sleep(int(settings['bitfinex_exporter']['interval']))
 
